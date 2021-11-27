72a7b7fe2033ce4771128efcbd87f7049c498cfa
Maruf Bin Salim <marufbinsalim22@gmail.com>
2021-11-27 23:18:02 +0600

    added styles for mobile, added search option (contextual) and also added settings page (not functional)

--- context/AppContext.js
+++ context/AppContext.js
@@ -1,14 +1,55 @@
 import { createContext, useContext, useState } from 'react';
+import { v4 as uuid } from 'uuid'
 
 export const AppContext = createContext();
 
 export function AppWrapper({ children }) {
 
   const [profileName, setProfileName] = useState("user");
+  const [profileID, setProfileID] = useState(uuid());
+  const [allThreadContext, setAllThreadContext] = useState([]);
+
+  let updateThreadContextFromBackend = async ()=>
+  {
+    let demo = [
+      {
+        userID: uuid(),
+        groupID: null,
+        threadName: "maruf",
+        threadPictureLink: ""
+      },
+      {
+        userID: uuid(),
+        groupID: null,
+        threadName: "farhan",
+        threadPictureLink: ""
+        
+      },
+      {
+        userID: null,
+        groupID: uuid(),
+        threadName: "cse group",
+        threadPictureLink: ""
+      },
+      {
+        userID: null,
+        groupID: uuid(),
+        threadName: "mat group",
+        threadPictureLink: ""
+      }
+    ]
+    // demo = [...demo, ...demo, ...demo, ...demo, ...demo, ...demo, ...demo, ...demo];
+    setAllThreadContext(demo);
+  }
 
   let sharedState = {
       profileName,
       setProfileName,
+      profileID,
+      setProfileID,
+      allThreadContext,
+      setAllThreadContext,
+      updateThreadContextFromBackend
   }
 
   return (
--- pages/chat/[to].js
+++ pages/chat/[to].js
@@ -13,13 +13,19 @@ const socket = io.connect(SOCKET_LINK);
 
 export default function Chat(props) {
 
-
-    let { profileName } = useAppContext();
-    let { isGroup, senderID, receiverID, groupID, threadName } = props;
+    let { profileName, profileID, allThreadContext, setAllThreadContext, updateThreadContextFromBackend } = useAppContext();
+    let { senderID, receiverID, groupID, threadName } = props;
     const [text, setText] = useState("");
     const [messages, setMessages] = useState([]);
+    const [optionPressed, setOptionPressed] = useState(false);
+    const [settingSelected, setSettingsSelected] = useState(false);
+    const [searchKeyword, selectSearchKeyword] = useState("");
+    const [searchContext, setSearchContext] = useState("all");
+    const [contextMessages, setContextMessages] = useState([]);
     let scrollTextRef = useRef();
     let inputBarRef = useRef();
+    threadName = "{ thread name }"
+    senderID = profileID;
 
 
     let getFormattedDateFromMillis = (millis) => {
@@ -27,16 +33,16 @@ export default function Chat(props) {
         return date.toLocaleString();
     }
 
-    let getThreadId = (senderID, recieverID, groupID, isGroup) => {
+    let getThreadId = (senderID, recieverID, groupID) => {
         let computed = "thread_";
-        if (isGroup) computed = groupID;
+        if (groupID) computed = groupID;
         else if (senderID < recieverID) computed = senderID + "_" + recieverID;
         else computed = recieverID + "_" + senderID;
         return computed;
     }
 
 
-    let threadID = getThreadId(senderID, receiverID, groupID, isGroup);
+    let threadID = getThreadId(senderID, receiverID, groupID);
 
     let sendMessage = async (event) => {
         if (text == "") return;
@@ -49,13 +55,46 @@ export default function Chat(props) {
             messageText: text,
             timestamp: Date.now(),
         });
-
         setText('');
     }
 
     let handleMessageBarChange = (event) => {
         setText(event);
         inputBarRef.current.focus();
+    }
+    let filterMessageContextByKeyword = (allContext, keyword, searchContext) => {
+        let filtered = [];
+        for (let context in allContext) {
+            let words = allContext[context]?.threadName.toLowerCase().split(' ');
+            let shouldAdd = false;
+            for (let word in words)
+                shouldAdd = shouldAdd || (words[word].indexOf(keyword) == 0);
+
+            shouldAdd = shouldAdd || (allContext[context].threadName.toLowerCase().indexOf(keyword) != -1 && keyword.length > 1);
+
+            if (!shouldAdd) continue;
+
+            let isAProfile = Object.entries(allContext[context])[0][1] != null;
+            let isAGroup = Object.entries(allContext[context])[1][1] != null;
+            if (isAProfile && (searchContext === "people" || searchContext === "all")) {
+                filtered = [...filtered, allContext[context]];
+            }
+            if (isAGroup && (searchContext === "group" || searchContext === "all")) {
+                filtered = [...filtered, allContext[context]];
+            }
+
+        }
+        return filtered;
+    }
+    let handleSearch = (event) => {
+        let keyword = event?.target?.value;
+        selectSearchKeyword(keyword);
+        if (!keyword.trim()) {
+            setContextMessages([]);
+            return;
+        }
+        let filteredUsers = filterMessageContextByKeyword(allThreadContext, keyword.toLowerCase(), searchContext);
+        setContextMessages(filteredUsers);
     }
 
     useEffect(() => {
@@ -67,33 +106,137 @@ export default function Chat(props) {
         });
 
         socket.on("recieve_message", (recievedMessage) => {
-            console.log("Recieved a new message from socket! \n");
+            console.log("Message from socket! \n");
             setMessages((messages) => [...messages, recievedMessage]);
         });
     }, [socket]);
 
-    useEffect(()=>
-    {
+    useEffect(async () => {
         inputBarRef.current.focus();
-
-    },[]);
+        await updateThreadContextFromBackend();
+    }, []);
 
     useEffect(() => {
-        scrollTextRef?.current?.scrollIntoView({behavior: 'smooth'});;
+        scrollTextRef?.current?.scrollIntoView({ behavior: 'smooth' });;
     }, [messages])
+
+    useEffect(() => {
+        if (!searchKeyword.trim()) {
+            setContextMessages([]);
+            return;
+        }
+        let filteredUsers = filterMessageContextByKeyword(allThreadContext, searchKeyword.toLowerCase(), searchContext);
+        setContextMessages(filteredUsers);
+    }, [searchContext])
 
 
     return (
         <>
+            <div className={`${styles.chat_options_backdrop} ${optionPressed ? styles.show_backdrop : styles.no_style}`}>
+                <div className={styles.backdrop_top_bar}>
+                    <div className={styles.backdrop_page_title_holder}>
+                        <p className={styles.backrop_page_title}>{threadName}</p>
+                    </div>
+                    <div className={styles.option_close_button_holder}>
+                        <button className={styles.option_close_button}>
+                            <img onClick={() => setOptionPressed(!optionPressed)} alt="" src={"../cross_icon.png"} />
+                        </button>
+                    </div>
+                </div>
+            </div>
             <div className={styles.chat_page}>
                 <div className={styles.chat_top_bar}>
-                    <p className={styles.page_title}>{threadName}Chat</p>
+                    <div className={styles.top_bar_left}>
+                        <div className={styles.home_button_holder}>
+                            <button className={styles.home_button}>
+                                <img onClick={() => { }} alt="" src={"../home_icon.png"} />
+                            </button>
+                        </div>
+                        <div className={styles.page_title_holder}>
+                            <p className={styles.page_title}>{"Chat"}</p>
+                        </div>
+                        <div className={styles.option_button_holder}>
+                            <button className={styles.option_button}>
+                                <img onClick={() => setOptionPressed(!optionPressed)} alt="" src={"../chat_option_icon.png"} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.top_bar_right}>
+                        <p className={styles.thread_title}>{threadName}</p>
+                    </div>
                 </div>
                 <div className={styles.chat_main}>
                     <div className={styles.chat_options}>
+                        <div className={styles.option_container}>
+                            <div className={styles.chat_option_container}>
+                                <div onClick={() => setSettingsSelected(false)} className={`${styles.search_option} ${!settingSelected ? styles.selected_option : styles.no_style}`}>
+                                    <img alt="" src={"../search_icon.png"} />
+                                </div>
+                                <div onClick={() => setSettingsSelected(true)} className={`${styles.settings_option} ${!settingSelected ? styles.no_style : styles.selected_option}`}>
+                                    <img alt="" src={"../chat_option_icon.png"} />
+                                </div>
+                            </div>
+                        </div>
+                        {
+                            settingSelected &&
+                            <>
+                            </>
+                        }
+                        {
+                            !settingSelected &&
+                            <>
+                                <div className={styles.searchbar_container}>
+                                    <input
+                                        type="text"
+                                        placeholder="Search"
+                                        className={styles.searchbar}
+                                        onChange={(e) => { handleSearch(e) }} />
+                                </div>
+
+                                <div className={styles.search_context_holder}>
+                                    <div onClick={() => setSearchContext("all")}
+                                        className={`${styles.search_context}
+                                    ${searchContext === "all" ? styles.selected_context : styles.no_style}`}>
+                                        <p className={`${styles.context_paragraph}`}>All</p>
+                                    </div>
+                                    <div onClick={() => setSearchContext("people")}
+                                        className={`${styles.search_context}
+                                    ${searchContext === "people" ? styles.selected_context : styles.no_style}`}>
+                                        <p className={`${styles.context_paragraph}`} >People</p>
+                                    </div>
+                                    <div onClick={() => setSearchContext("group")}
+                                        className={`${styles.search_context}
+                                    ${searchContext === "group" ? styles.selected_context : styles.no_style}`}>
+                                        <p className={`${styles.context_paragraph}`}>Group</p>
+                                    </div>
+                                </div>
+
+                                <div className={styles.search_result_container}>
+                                    {
+                                        contextMessages.map((context, index) => {
+                                            return (
+
+                                                <div className={styles.search_result} key={index || context.userID || context.groupID}>
+                                                    <div className={styles.context_profile_picture_container}>
+                                                        <img alt="" src={"../default_profile_pic.jpg"} />
+                                                    </div>
+                                                    <div className={styles.context_threadname_container}>
+                                                        <p>   {context?.threadName} </p>
+                                                    </div>
+                                                    <div className={styles.goto_button_container}>
+                                                        <img alt="" src={"../chat_option_icon.png"} />
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+
+                            </>
+                        }
                     </div>
                     <div className={styles.chat_feed}>
-                        <div className={styles.chat_list}>
+                        <div className={`${styles.chat_list}   ${optionPressed ? styles.invinsible : styles.no_style}`}>
                             {
                                 messages.map((currentMessage) => {
                                     return (
